71d96fbde5ca5f03cc2d7500ba6d83402d6b0b05
nrifath2009 <Mahmuda@PLACOVU-WS-3>
2018-01-18 12:19:07 +0600

    Factory Method Example Implemented

--- FactoryMethod/Program.cs
+++ FactoryMethod/Program.cs
@@ -10,6 +10,55 @@ namespace FactoryMethod
     {
         static void Main(string[] args)
         {
+            var factory = new SavingsAccountFactory() as ICreditUnionFactory;
+            var citiAccount = factory.GetSavingsAccount("CITI-321");
+            var nationalAccount = factory.GetSavingsAccount("NATIONAL-345");
+            Console.WriteLine($"My city balance is ${citiAccount.Balance} and national balance is ${nationalAccount.Balance}");
+            Console.ReadKey();
+        }
+    }
+
+    public abstract class ISavingsAccount
+    {
+        public decimal Balance { get; set; }
+
+    }
+
+    public class CitiSavingsAccount : ISavingsAccount
+    {
+        public CitiSavingsAccount()
+        {
+            Balance = 5000;
+        }
+    }
+
+    public class NationalSavingsAccount : ISavingsAccount
+    {
+        public NationalSavingsAccount()
+        {
+            Balance = 2000;
+        }
+    }
+
+    public interface ICreditUnionFactory
+    {
+        ISavingsAccount GetSavingsAccount(string accountNo);
+    }
+
+    public class SavingsAccountFactory : ICreditUnionFactory
+    {
+        public ISavingsAccount GetSavingsAccount(string accountNo)
+        {
+            if (accountNo.Contains("CITI"))
+            {
+                return new CitiSavingsAccount();
+            }
+            else if (accountNo.Contains("NATIONAL"))
+            {
+                return new NationalSavingsAccount();
+            }
+            else
+                throw new ArgumentException("Invalid Account Number");
         }
     }
 }
